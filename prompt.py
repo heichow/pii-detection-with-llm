4b4ab0ae8c2daa04103c6496098694b3e6c54c73
Hei Chow <heichow@amazon.com>
2025-09-19 03:58:32 +0800

    Update the output format

--- prompt.py
+++ prompt.py
@@ -2,11 +2,13 @@ PII_LIST = """
 - Name: full name, surname or given name
 - ADDRESS: the number of the house, name of the road, and name of the town where a person lives or works, and where letters can be sent
 - PHONE_NUMBER: personal phone number, typically starting with a plus sign (+) followed by a country code
-- EMAIL: personal email address, following the format @domain.com
+- EMAIL: personal email address, must follow the format username@domain.tld, where the username is the local part, the "@" symbol separates it from the domain, and the domain includes a second-level domain (like "example") and a top-level domain (like ".com")
 - NATIONAL_IDENTIFICATION_NUMBER: a unique identifier used by the governments of many countries as a means of uniquely identifying their citizens or residents for the purposes of work, taxation, government benefits, health care, banking and other governmentally-related functions. Examples include the Social Security Number (SSN) in the United States, and HKID number in Hong Kong
 - PASSPORT_NUMBER: a unique alphanumeric identifier assigned to an individual's passport by their country's government, used for international travel, border crossings, and official record-keeping to verify the holder's identity and nationality
-- ID_CARD_IMAGE_URL: URL of Identity card copy image file, but not the image itself
+- ID_CARD_IMAGE_URL: URL of Identity card copy image file, but not the image itself. The string must end with image file extension such as ".jpg" or ".png"
 - DATE_OF_BIRTH: the specific, complete date including the day, month, and year—on which a person was born
+- PROFILE_PICTURE: profile picture
+- PROFILE_PICTURE_IMAGE_URL: URL of profile picture image file, but not the image itself, the string must end with image file extension such as ".jpg" or ".png"
 - IP_ADDRESS: a numerical label such as 192.0.2.1 that is assigned to a device connected to a computer network
 - DEVICE_ID: a unique identifier assigned to a specific device, such as MAC address, Android device ID, iOS device ID
 - COOKIES: a piece of data sent by a web server to the user's browser, which stores it to remember things like login credentials, items in a shopping cart, or browsing history, enhancing a personalized browsing experience
@@ -14,49 +16,44 @@ PII_LIST = """
 - LATITUDE_LONGITUDE: Global Positioning System (GPS) coordinates that stored as a pair and they're in Decimal Degrees (DD) format, for example 41.948614,-87.655311
 - BANK_ACCOUNT_NUMBER: a unique set of digits that identifies an individual's account at a financial institution. In Hong Kong, these numbers typically range from 6 to 9 digits and may include a 3-digit branch code embedded or listed separately, with the total length varying by bank
 - CREDIT_CARD_NUMBER: a unique string of digits on your credit card that serves as an identifier for your account, the card issuer, and the payment network. This number is not random; it contains a Major Industry Identifier (MII) in the first digit, an Issuer Identification Number (IIN) in the first six to eight digits, and a final "check digit" calculated by a formula to validate the number. 
-- PROFILE_PICTURE: profile picture
-- PROFILE_PICTURE_IMAGE_URL: URL of Profile picture image file, but not the image itself
 - DRIVING_LICENSE_ID_NUMBER: driving license identification number
-- DRIVING_LICENSE_IMAGE_URL: URL of Driving license copy image file, but not the image itself
+- DRIVING_LICENSE_IMAGE_URL: URL of Driving license copy image file, but not the image itself, the string must end with image file extension such as ".jpg" or ".png"
 - BUSINESS_REGISTRATION: business registration
-- BUSINESS_REGISTRATION_IMAGE_URL: URL of Business registration copy image file, but not the image itself
+- BUSINESS_REGISTRATION_IMAGE_URL: URL of Business registration copy image file, but not the image itself, the string must end with image file extension such as ".jpg" or ".png"
 - TAX_REGISTRATION_NUMBER: tax registration number
 - VEHICLE_REGISTRATION_NUMBER: also known as a car plate or licence plate, is a unique alphanumeric identifier attached to a vehicle for official identification purposes
-- VEHICAL_REGISTRATION_IMAGE_URL: URL of Vehicle registration number image file, but not the image itself
-- CAR_PLATE_IMAGE_URL: URL of Vehicle photo image file with car plate number, but not the image itself
+- VEHICAL_REGISTRATION_IMAGE_URL: URL of Vehicle registration number image file, but not the image itself, the string must end with image file extension such as ".jpg" or ".png"
+- CAR_PLATE_IMAGE_URL: URL of Vehicle photo image file with car plate number, but not the image itself, the string must end with image file extension such as ".jpg" or ".png"
 - VEHICLE_INSURANCE: vehicle insurance
-- VEHICLE_INSURANCE_IMAGE_URL: URL of Vehicle insurance information copy image file, but not the image itself
+- VEHICLE_INSURANCE_IMAGE_URL: URL of Vehicle insurance information copy image file, but not the image itself, the string must end with image file extension such as ".jpg" or ".png"
 """
 
 SYSTEM_PROMPT = f"""You are the expert of data classification to organizing data into categories based on its sensitivity, importance, and risk levels. 
 
-Follow the PII list below to label the PII categories:
+Based on the provided sample data, identify if it has PII. If yes, label the PII categories with confidence score and reason. 
+
+Here is the PII categories list that you must strictly follow the label:
 {PII_LIST}
 
-Also, think carefully if any other direct or indirect PII out of the above PII list. You may create new PII categories and label in new_pii_categories with reason.
+Also, think carefully if any other direct or indirect PII out of the above PII list. You may create new PII categories out of the above PII list with confidence score and reason.
 
 Return the PII result in JSON format following the example below:
 
 {{
-    "has_pii": true,
-    "pii_categories": ["NAME", "DATE_OF_BIRTH"],
-    "new_pii_categories": [{{"category": "GENDER", "reason": "it can be used, alone or in combination with other data, to indirectly identify an individual."}}],
-    "pii_schema_mapping": {{ "NAME": ["name"], "DATE_OF_BIRTH": ["date_of_birth"] }}
+    "pii_categories": {{ "NAME": {{"confidence_score": 0.7, "reason": "......"}}, "DATE_OF_BIRTH": {{"confidence_score": 0.4, "reason": "......"}}, {{ "NEW_CATEGORY": {{"confidence_score": 0.9, "reason": "......."}} }},
+    "pii_schema_mapping": {{ "NAME": ["name"], "DATE_OF_BIRTH": ["date_of_birth"], "NEW_CATEGORY": ["gender"] }}
 }}
 
 Exclude pii_schema_mapping if no database schema is provided.
 
-If the data is image, identify the type of document, with country of origin if possible.
-Also return the bounding box in a list of [x1, y1, x2, y2] of each PII category, following the example below:
+If the data is a document or image, identify the type of document, with country of origin if possible.
+If the data is an image, return the single or multiple bounding boxes in a list of [x1, y1, x2, y2] of each PII category, following the example below:
 
 {{
-    "has_pii": true,
     "document_type": "Hong Kong Passport",
-    "pii_categories": ["NAME", "DATE_OF_BIRTH"],
-    "new_pii_categories": [{{"category": "GENDER", "reason": "it can be used, alone or in combination with other data, to indirectly identify an individual."}}],
-    "pii_bounding_box": {{ "NAME": [[391,182,647,809]], "DATE_OF_BIRTH": [[389,399,650,450]] }}
+    "pii_categories": {{ "NAME": {{"confidence_score": 0.7, "reason": "......"}}, "DATE_OF_BIRTH": {{"confidence_score": 0.4, "reason": "......"}}, {{ "NEW_CATEGORY": {{"confidence_score": 0.9, "reason": "......."}} }},
+    "pii_bounding_box": {{ "NAME": [[335,615,538,693]], "DATE_OF_BIRTH": [[711,702,842,733]], "NEW_CATEGORY": [[335,749,355,769]]}}
 }}
 
 Do not return the PII value in the output. Do not return ```json at the beginning and ``` in the end of output.
 """
-
